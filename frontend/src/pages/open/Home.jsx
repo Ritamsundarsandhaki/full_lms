6d2a4ce9fc482788ecf7e7a34ac46027c292da43
karpritam <karpritam194@gmail.com>
2025-02-16 19:47:59 +0530

    add 1st image in home page

--- frontend/src/pages/open/Home.jsx
+++ frontend/src/pages/open/Home.jsx
@@ -1,44 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "/src/css/home.css";
+// import ImageWithAnimation, from "/src/js/home";
 
 const Home = () => {
 	return (
-		<div id="header">
-			<div className="container">
-				<nav>
-					<ul id="menu">
-						<li>
-							<a href="#home">Home</a>
-						</li>
-						<li>
-							<a href="#login">Login</a>
-						</li>
-						<p id="middle">NIRT</p>
-						<li>
-							<a href="#register">Register</a>
-						</li>
-						<li>
-							<a href="#getStarted">Get Started</a>
-						</li>
-						<p id="login">
-							<a href="#login">Log in</a>
+		<div className="display">
+			<div id="header">
+				<div className="container">
+					<nav>
+						<ul id="menu">
+							<li>
+								<a href="#header">Home</a>
+							</li>
+							<li>
+								<a href="#login">Login</a>
+							</li>
+							<p id="middle">NIRT</p>
+							<li>
+								<a href="#register">Register</a>
+							</li>
+							<li>
+								<a href="#getStarted">Get Started</a>
+							</li>
+							<p id="login">
+								<a href="#login">Log in</a>
+							</p>
+						</ul>
+					</nav>
+				</div>
+				<div className="info">
+					<div className="header-text">
+						<p>Welcome To Our NIRT</p>
+						<p>Library</p>
+						<p id="info-msg">
+							Your one-stop solution for managing your library effectively
 						</p>
-					</ul>
-				</nav>
+					</div>
+					<div className="search-bar">
+						<input type="text" placeholder="Search Book" />
+						<button>Join Us</button>
+					</div>
+				</div>
 			</div>
-			<div className="info">
-				<div className="header-text">
-					<p>Welcome To Our NIRT</p>
-					<p>Library</p>
-					<p id="info-msg">
-						Your one-stop solution for managing your library effectively
-					</p>
-				</div>
-				<div className="search-bar">
-					<input type="text" placeholder="Search Book" />
-					<button>Join Us</button>
-				</div>
+			<div className="image">
+				<span> <img className="fade-in" src="./homePage_img/library_img1" alt="Library Image"></img></span>
 			</div>
 		</div>
 	);
